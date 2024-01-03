a50d440fe8d305d2c1fbf754a5e7fe936632eafa
Adrian Kevin Viola <adrianviola21@gmail.com>
2024-01-03 16:17:12 +0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+    if (name === '' || email === '' || phone === '' || message === '') {
+        alert('Please fill out all fields before submitting the form.');
+        return false;
+    }
+
+    window.location.href = 'submit_page.html';
+
+    return false;
+};
